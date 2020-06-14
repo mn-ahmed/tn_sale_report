9ea09b0d40a4197d7e093f4c0fd70efbe4cb3000
mn-ahmed <ahmedmnasri@gmail.com>
2020-06-14 02:43:56 +0100

    Clean code

--- __manifest__.py
+++ __manifest__.py
@@ -3,42 +3,24 @@
 
 {
     'name': "TN sale Report",
-
     'summary': """Tunisia based sale report""",
-
     'description': """
         ...
     """,
-
     'license': 'AGPL-3',
-
     'author': "	AHMED MNASRI",
-
     'website': "",
-
     'category': 'Sale',
-
-    'version': '12.0.0.0.0',
-
+    'version': '12.0.0.1.0',
     'depends': ['sale', ],
-
     'data': [
             #'views/sale_view.xml',
             'report/reports.xml',
             'report/sale_report_template.xml',
-
              ],
 
-    'images': [
-        'static/description/banner.png',
-        'static/description/icon.png',
-        'static/description/index.html',
-    ],
-
     'installable': True,
-
     'auto_install': False,
-
     'application': True,
 
 }
